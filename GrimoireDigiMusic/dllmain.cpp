12b6e2e93b41192f747a1e7f874f1ce92934001a
Solanacean <winterfury7@gmail.com>
2019-02-15 00:38:08 +0300

    Do not notify user about missing system dsound.dll as it's probably not a safe thing to do in DllMain.

--- GrimoireDigiMusic/dllmain.cpp
+++ GrimoireDigiMusic/dllmain.cpp
@@ -15,7 +15,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
     {
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hModule);
-        Logger::Instance().Init("DIGIMUSIC_LOG.txt", Logger::Level::warning);
 
         {
             TCHAR szPath[MAX_PATH];
@@ -24,11 +23,11 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
             DWORD dwAttrib = GetFileAttributes(szPath);
             if (dwAttrib == INVALID_FILE_ATTRIBUTES || dwAttrib & FILE_ATTRIBUTE_DIRECTORY)
             {
-                LOG_FATAL("System dsound.dll is missing. If you're using WINE, install the native dsound.dll using Winetricks and configure WINE to use the installed version instead of the built-in one.");
                 return FALSE;
             }
         }
 
+        Logger::Instance().Init("DIGIMUSIC_LOG.txt", Logger::Level::warning);
         InstallAPIHooks(); 
         break;
 
